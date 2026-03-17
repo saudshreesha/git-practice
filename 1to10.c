934a331c74b29ebc94e9297be89a2e50a74204f1
Shreesha Saud <shreeshasaud295@gmail.com>
2026-03-17 10:39:36 +0545

    Added 1to10.c from Desktop

---
+++ 1to10.c
@@ -0,0 +1,19 @@
+/* WAP to print
+1
+2 3
+4 5 6
+7 8 9 10*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i, j, num=1;
+	for(i=1; i<=4; i++){
+		for(j=1; j<=i; j++){
+	printf("%d ", num);
+	num++;
+	}
+	printf("\n");
+	}
+	getch();
+} 
